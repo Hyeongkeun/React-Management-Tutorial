9b796bcf21243a31bc641db948a3e3e2eac97a41
Hyeongkeun Lee <lhk528@gmail.com>
2021-07-20 07:14:39 +0000

    Create form button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -78,42 +79,46 @@ class App extends Component{
   render(){
     const { classes } = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {/* this.state.customers 값은 초기에는 ""이므로 err가 난다. 따라서 값이 존재할 때만 map을 할 수 있도록 this.state.customers ?를 붙혀준다.*/}
-            {this.state.customers ? this.state.customers.map(c => {
-              return(
-                <Customer
-                  key={c.id} //map을 사용하기 위해서는 key 값이 필요
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            }) : 
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-            </TableRow>
-            } 
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {/* this.state.customers 값은 초기에는 ""이므로 err가 난다. 따라서 값이 존재할 때만 map을 할 수 있도록 this.state.customers ?를 붙혀준다.*/}
+              {this.state.customers ? this.state.customers.map(c => {
+                return(
+                  <Customer
+                    key={c.id} //map을 사용하기 위해서는 key 값이 필요
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              } 
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+      </div>
+
     );
   }
 }
