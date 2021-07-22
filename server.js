5f8763dcfcf5f827be3d6a4a00cd6dc8c0448f0e
Hyeongkeun Lee <lhk528@gmail.com>
2021-07-22 15:11:49 +0000

    Update UI/UX & Add Customer Search Module

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
     let sql = `INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)`;
+    console.log(req.file);
     let image = '/image/' + req.file.filename; //multer 라이브러리가 자동으로 이름을 겹치지 않게 한다.
     let name = req.body.name;
     let birthday = req.body.birthday;
